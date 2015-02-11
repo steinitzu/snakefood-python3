82ab674b3a1588c742f1355495e9c2ec6e03708d
neothemachine <maik.riechert@arcor.de>
2015-02-11 10:03:47 +0100

    added -f parameter to cluster_regexp tool

--- lib/python/snakefood/cluster_regexp.py
+++ lib/python/snakefood/cluster_regexp.py
@@ -10,6 +10,7 @@ import sys
 import re
 from collections import defaultdict
 
+from snakefood.six.moves import map
 from snakefood.depends import read_depends, output_depends
 
 
@@ -18,11 +19,30 @@ def iterpairs(thelist):
     while 1:
         yield next(ilist), next(ilist)
 
+def read_clusters(fn):
+    "Return a list of cluster regexes read from the file 'fn'."
+    f = open(fn, 'rU')
+    clusters = []
+    for x in map(str.strip, f.xreadlines()):
+        if not x:
+            continue
+        ws = x.index(' ')
+        regex, rename = x[:ws], x[ws+1:]
+        clusters.append(regex)
+        clusters.append(rename)
+    return clusters
 
 def main():
     import optparse
-    parser = optparse.OptionParser(__doc__.strip())
+    parser = optparse.OptionParser()
+    
+    parser.add_option('-f', '--from-file', action='store',
+                      help="Read cluster list from the given filename.")
+    
     opts, renames = parser.parse_args()
+
+    if opts.from_file:
+        renames.extend(read_clusters(opts.from_file))
 
     if len(renames) % 2:
         parser.error("An odd number of renames was specified.")
